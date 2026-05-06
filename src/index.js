c809ffb5cd01b3e624f59388cade52fe7581ae9e
Kaisar <kaisar970225@gmail.com>
2026-05-06 23:36:13 +0500

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
-};
+const ones = [
+  '',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'ten',
+  'eleven',
+  'twelve',
+  'thirteen',
+  'fourteen',
+  'fifteen',
+  'sixteen',
+  'seventeen',
+  'eighteen',
+  'nineteen',
+];
+
+const tens = [
+  '',
+  '',
+  'twenty',
+  'thirty',
+  'forty',
+  'fifty',
+  'sixty',
+  'seventy',
+  'eighty',
+  'ninety',
+];
+
+function toReadable(number) {
+  if (number === 0) return 'zero';
+
+  if (number < 20) {
+    return ones[number];
+  }
+
+  if (number < 100) {
+    const ten = tens[Math.floor(number / 10)];
+    const one = ones[number % 10];
+    return one ? `${ten} ${one}` : ten;
+  }
+
+  if (number < 1000) {
+    const hundred = ones[Math.floor(number / 100)];
+    const rest = number % 100;
+    return rest
+      ? `${hundred} hundred ${toReadable(rest)}`
+      : `${hundred} hundred`;
+  }
+
+  return '';
+}
+
+module.exports = toReadable;
